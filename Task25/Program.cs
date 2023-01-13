fe05937d46dff597d66d07e47751923551ce0351
Irina <liamina2207@gmail.com>
2023-01-13 10:04:08 +0300

    Added .gitignore

---
+++ Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который 
+// принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите целое число A: ");
+Console.Write("Введите целое число B: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiationNumbers = ExponentiationNumbers(number);
+Console.WriteLine($"число А {numberA} в степени числа B {numberB} = {exponentiationNumbers}");
+
+void ExponentiationNumbers(int numA, int numB)
+{
+    int exp = 0;
+    for (numB > 0;)
+    {
+        exp = numA * numB;
+    }
+    return exp;
+}
---
+++ Task29/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+// array[0] = 1;
+// array[1] = 0;
+// array[2] = 1;
+// array[3] = 1;
+// array[4] = 0;
+// array[5] = 1;
+// array[6] = 0;
+// array[7] = 0;
+
+// FillArray(array);
+// PrintArray(array);
+
+int[] array = CreateArray(8);
+PrintArray(array);
+
+int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+    arr[i] = rnd.Next(0, 34); 
+    }
+}
+
+int[] PrintArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+    Console.Write(arr[i] + " ,"); 
+    }
+}
+Console.WriteLine(); 
