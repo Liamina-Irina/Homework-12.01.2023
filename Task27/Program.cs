ee6e61c71d2f4d3969b93295d46123cde33b55cb
Irina <liamina2207@gmail.com>
2023-01-18 07:11:02 +0300

    Solved Task 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -4,3 +4,28 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumDigitNumbers = SumDigitNumbers(number);
+
+int SumDigitNumbers(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+    int i = num % 10;    
+       sum +=i;
+       num = num / 10;
+    }
+    return sum;
+}
+
+if (number > 0)
+{
+    Console.WriteLine($"Сумма цифр в числе {number} = {sumDigitNumbers}");
+}
+else
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
